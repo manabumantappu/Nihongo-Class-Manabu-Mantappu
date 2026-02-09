fb9261c1226788b600bf5617be602901f964ef4d
manabumantappu <azishachigo@gmail.com>
2026-02-09 19:56:23 +0700

    Update materi.js

--- lms-nihongo/assets/js/materi.js
+++ lms-nihongo/assets/js/materi.js
@@ -1,1 +1,10 @@
-console.log('Materi JS loaded');
+console.log("Materi module loaded");
+
+// nanti bisa dihubungkan ke Firebase / API
+const materi = [
+  { judul: "Hiragana Dasar", tipe: "PDF" },
+  { judul: "Katakana", tipe: "PDF" },
+  { judul: "Kanji N5", tipe: "PPT" }
+];
+
+console.table(materi);
