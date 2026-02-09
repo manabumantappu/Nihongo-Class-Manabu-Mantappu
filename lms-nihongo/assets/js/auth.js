fcd89e03a774423dc5ec60f2b1fcae913e347ec3
manabumantappu <azishachigo@gmail.com>
2026-02-09 19:55:22 +0700

    Update auth.js

--- lms-nihongo/assets/js/auth.js
+++ lms-nihongo/assets/js/auth.js
@@ -1,10 +1,29 @@
 function login() {
-  const email = document.getElementById('email').value;
+  const email = document.getElementById("email").value.trim();
 
-  let role = 'siswa';
-  if (email.includes('guru')) role = 'guru';
-  if (email.includes('admin')) role = 'admin';
+  if (!email) {
+    alert("Masukkan email");
+    return;
+  }
 
-  localStorage.setItem('role', role);
-  location.href = 'dashboard.html';
+  let role = "siswa";
+  if (email.includes("guru")) role = "guru";
+  if (email.includes("admin")) role = "admin";
+
+  localStorage.setItem("auth", JSON.stringify({
+    email,
+    role
+  }));
+
+  // redirect
+  if (role === "admin") {
+    location.href = "admin/index.html";
+  } else {
+    location.href = "dashboard.html";
+  }
+}
+
+function logout() {
+  localStorage.clear();
+  location.href = "../index.html";
 }
