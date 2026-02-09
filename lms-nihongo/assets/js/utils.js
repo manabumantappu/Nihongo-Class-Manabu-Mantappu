ac5b685458a03ae2779bb42278172dcd72e59211
manabumantappu <azishachigo@gmail.com>
2026-02-09 19:56:38 +0700

    Update utils.js

--- lms-nihongo/assets/js/utils.js
+++ lms-nihongo/assets/js/utils.js
@@ -1,3 +1,10 @@
-export function now() {
-  return new Date().toLocaleString();
+export function formatDate(date = new Date()) {
+  return date.toLocaleDateString("id-ID");
+}
+
+export function formatTime(date = new Date()) {
+  return date.toLocaleTimeString("id-ID", {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
 }
