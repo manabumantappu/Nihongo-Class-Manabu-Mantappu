96681462e736e7b78709279e10d162a697e59b9a
manabumantappu <azishachigo@gmail.com>
2026-02-09 21:22:39 +0700

    Update presensi.js

--- assets/js/presensi.js
+++ assets/js/presensi.js
@@ -2,18 +2,49 @@ const masuk = document.getElementById("masuk");
 const pulang = document.getElementById("pulang");
 const table = document.getElementById("table");
 
+let data = JSON.parse(localStorage.getItem("presensiData")) || [];
+const email = localStorage.getItem("email") || "unknown";
+
+function save() {
+  localStorage.setItem("presensiData", JSON.stringify(data));
+}
+
+function renderUser() {
+  table.innerHTML = "";
+  data
+    .filter(d => d.email === email)
+    .forEach(d => {
+      table.innerHTML += `
+        <tr class="border-t">
+          <td class="p-2">${d.email}</td>
+          <td class="p-2">${d.waktu}</td>
+          <td class="p-2">${d.status}</td>
+        </tr>`;
+    });
+}
+
+function now() {
+  return new Date().toLocaleString("id-ID");
+}
+
 masuk?.addEventListener("click", () => {
-  table.innerHTML += `
-    <tr class="border-t">
-      <td class="p-2">${new Date().toLocaleString()}</td>
-      <td class="p-2 text-green-600">Masuk</td>
-    </tr>`;
+  data.push({
+    email,
+    waktu: now(),
+    status: "Masuk"
+  });
+  save();
+  renderUser();
 });
 
 pulang?.addEventListener("click", () => {
-  table.innerHTML += `
-    <tr class="border-t">
-      <td class="p-2">${new Date().toLocaleString()}</td>
-      <td class="p-2 text-blue-600">Pulang</td>
-    </tr>`;
+  data.push({
+    email,
+    waktu: now(),
+    status: "Pulang"
+  });
+  save();
+  renderUser();
 });
+
+renderUser();
