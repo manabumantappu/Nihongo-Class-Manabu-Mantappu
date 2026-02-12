8e757b7618e65a471f8c8ad26b4c6b0b22672e50
manabumantappu <azishachigo@gmail.com>
2026-02-13 00:15:45 +0700

    Create qr-generate.js

---
+++ assets/js/qr-generate.js
@@ -0,0 +1,19 @@
+function getToday() {
+  return new Date().toISOString().split("T")[0];
+}
+
+window.generateQR = function() {
+
+  const token = {
+    tanggal: getToday(),
+    kelas: "JP-NIHONGO"
+  };
+
+  document.getElementById("qrcode").innerHTML = "";
+
+  new QRCode(document.getElementById("qrcode"), {
+    text: JSON.stringify(token),
+    width: 250,
+    height: 250
+  });
+};
