59ba19643e67569c04ecac3f30ee3237fc07b239
manabumantappu <azishachigo@gmail.com>
2026-02-15 20:59:12 +0700

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", () => {
 // Tambahkan ini di luar DOMContentLoaded
 window.logout = function () {
   localStorage.clear();
-  window.location.href = "../login.html";
+  window.location.href = "../index.html";
 };
 
 
